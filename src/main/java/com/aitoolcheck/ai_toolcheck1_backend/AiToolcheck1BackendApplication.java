9ff84290376fb4c1e646ba586092c2d295beef2f
Cao Hoang Lam <caol69265@gmail.com>
2026-04-20 14:26:08 +0700

    chore: initial commit backend source code

---
+++ src/main/java/com/aitoolcheck/ai_toolcheck1_backend/AiToolcheck1BackendApplication.java
@@ -0,0 +1,13 @@
+package com.aitoolcheck.ai_toolcheck1_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiToolcheck1BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiToolcheck1BackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aitoolcheck/ai_toolcheck1_backend/AiToolcheck1BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aitoolcheck.ai_toolcheck1_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiToolcheck1BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
