5f632b93849f0162562a9f8613c683ee8968302f
VoMinhDuc1311 <minhducvo140201@gmail.com>
2026-04-23 09:38:25 +0700

    feat: add ai and log dto classes

--- src/main/java/com/aitoolcheck/ai_toolcheck1_backend/dto/aiskill/req/CreateAiSkillRequest.java
+++ src/main/java/com/aitoolcheck/ai_toolcheck1_backend/dto/aiskill/req/CreateAiSkillRequest.java
@@ -1,4 +1,20 @@
 package com.aitoolcheck.ai_toolcheck1_backend.dto.aiskill.req;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class CreateAiSkillRequest {
+    private String skillCode;
+    private String skillName;
+    private String description;
 }
--- src/main/java/com/aitoolcheck/ai_toolcheck1_backend/dto/legacyinferencelog/req/UpdateLegacyInferenceLogRequest.java
+++ src/main/java/com/aitoolcheck/ai_toolcheck1_backend/dto/legacyinferencelog/req/UpdateLegacyInferenceLogRequest.java
@@ -1,4 +1,20 @@
 package com.aitoolcheck.ai_toolcheck1_backend.dto.legacyinferencelog.req;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class UpdateLegacyInferenceLogRequest {
+
+    private String inferredMetadataJson;
+    private BigDecimal confidenceScore;
 }
