b1f1bbe2462ad7276f376832ec21ad58d412d45b
VoMinhDuc1311 <minhducvo140201@gmail.com>
2026-05-07 09:01:08 +0700

    feat: add test runner repository queries

--- src/main/java/com/aitoolcheck/ai_toolcheck1_backend/repository/TestRunRepository.java
+++ src/main/java/com/aitoolcheck/ai_toolcheck1_backend/repository/TestRunRepository.java
@@ -1,4 +1,14 @@
 package com.aitoolcheck.ai_toolcheck1_backend.repository;
 
-public interface TestRunRepository {
+import com.aitoolcheck.ai_toolcheck1_backend.model.TestRun;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+public interface TestRunRepository extends JpaRepository<TestRun, UUID> {
+
+    List<TestRun> findBySourceProject_IdOrderByCreatedAtDesc(UUID projectId);
 }
