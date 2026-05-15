cfba62c223a215fb860c9dab393a9410ae9c0c47
VoMinhDuc1311 <minhducvo140201@gmail.com>
2026-05-15 11:12:16 +0700

    feat: add test run realtime publisher

---
+++ src/main/java/com/aitoolcheck/ai_toolcheck1_backend/enums/TestRunRealtimeEventType.java
@@ -0,0 +1,9 @@
+package com.aitoolcheck.ai_toolcheck1_backend.enums;
+
+public enum TestRunRealtimeEventType {
+    RUN_STARTED,
+    ITEM_STARTED,
+    ITEM_COMPLETED,
+    RUN_COMPLETED,
+    RUN_FAILED
+}
